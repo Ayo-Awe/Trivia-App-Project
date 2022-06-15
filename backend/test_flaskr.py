fb536d1a749df3806b3e60258321da0bc6d43d72
Awe <pupoawe@icloud.com>
2022-06-15 15:56:54 +0100

    test: tests categories endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format("postgres","Aweayo",'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +33,26 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    #tests for get categories endpoint
+
+    def test_get_all_categories(self):
+        res = self.client().get('/api/v1/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code,200) # status code is 200
+        self.assertEqual(data["success"],True) # the value of success is set to True
+        self.assertTrue(len(data["categories"])) # categories exist
+        pass
+
+    def test_405_for_invalid_category_endpoint(self):
+        res = self.client().get('/api/v1/categories/3')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code,405)
+        self.assertEqual(data["success"],False)
+        self.assertTrue(data["error"])
+        self.assertTrue(data["message"])
+        pass
 
 
 # Make the tests conveniently executable
