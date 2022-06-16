5a7bf2f332ff46651c3311d39333fe0ca10320ba
Awe <pupoawe@icloud.com>
2022-06-16 16:50:49 +0100

    chore: modifies question search endpoint to include /api/v1

--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -84,7 +84,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/api/v1/questions`, //TODO: update request URL
       type: "POST",
       dataType: "json",
       contentType: "application/json",
